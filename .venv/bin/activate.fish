16c1d837800e995f9110fefba096768d1565af6d
Suraj Tiwari <your-emailSurajTiwari.edu@yandex.com>
2026-07-05 21:46:31 +0530

    new

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ramanujan/Desktop/OSINT-Streamlit-main/.venv
+set -gx VIRTUAL_ENV /home/ramanujan/Desktop/DigiHAWK/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
